860778f614f611a4106249af24655bf370dfbeaa
ErikToala <183404@ids.upchiapas.edu.mx>
2020-12-11 04:23:24 -0600

    Vista del cliente y del server

--- ClientFrogger/src/ClientFrogger/View/ControllerGameClient.java
+++ ClientFrogger/src/ClientFrogger/View/ControllerGameClient.java
@@ -2,24 +2,37 @@ package ClientFrogger.View;
 
 import ClientFrogger.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
-public class ControllerGameClient {
-    @FXML
-    private AnchorPane pane;
+import java.net.URL;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.ResourceBundle;
 
-    @FXML
-    private ImageView serpentYellow;
+public class ControllerGameClient implements Observer {
+    @FXML private AnchorPane pane;
+    @FXML private ImageView serpentYellow;
+    @FXML private ImageView rightTrunk;
+    @FXML private ImageView leftTrunk;
+    @FXML private ImageView yellowCar2;
+    @FXML private ImageView redCar2;
+    @FXML private ImageView whiteCar2;
+    @FXML private ImageView centerTrunk;
+    @FXML private ImageView redCar1;
+    @FXML private ImageView whiteCar1;
+    @FXML private ImageView yellowCar1;
 
-    @FXML
-    private ImageView rightTrunk;
-
-    @FXML
-    private ImageView leftTrunk;
     private Main main;
+    private ImageView[] obstacles = new ImageView[10];
 
     public void setMain(Main main) {
         this.main = main;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
     }
 }
--- ClientFrogger/src/ClientFrogger/View/ControllerGameServer.java
+++ ClientFrogger/src/ClientFrogger/View/ControllerGameServer.java
@@ -5,25 +5,32 @@ import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
-public class ControllerGameServer {
-    @FXML
-    private AnchorPane pane;
+import java.util.Observable;
+import java.util.Observer;
 
-    @FXML
-    private ImageView serpentYellow;
-
-    @FXML
-    private ImageView rightTrunk;
-
-    @FXML
-    private ImageView leftTrunk;
-
-    @FXML
-    private ImageView frogImg;
+public class ControllerGameServer implements Observer {
+    @FXML private AnchorPane pane;
+    @FXML private ImageView serpentYellow;
+    @FXML private ImageView rightTrunk;
+    @FXML private ImageView leftTrunk;
+    @FXML private ImageView frogImg;
+    @FXML private ImageView yellowCar2;
+    @FXML private ImageView redCar2;
+    @FXML private ImageView whiteCar2;
+    @FXML private ImageView centerTrunk;
+    @FXML private ImageView redCar1;
+    @FXML private ImageView whiteCar1;
+    @FXML private ImageView yellowCar1;
 
     private Main main;
+    private ImageView[] obstacles = new ImageView[10];
 
     public void setMain(Main main) {
         this.main = main;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
     }
 }
